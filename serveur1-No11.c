dfe7b82de68461d2eb1b8a9865dcda9efd199198
StevenINSA <>
2020-12-13 20:18:12 +0100

    test débit

--- serveur1-No11.c
+++ serveur1-No11.c
@@ -232,10 +232,10 @@ int main(int argc, char* argv[]){
 
     sendto(data_descriptor,bufferUDP_write_server,sizeof(bufferUDP_write_server),0,(struct sockaddr *)&client1_addr,len);
 
-    time_debit.tv_usec = (time_debit_end.tv_sec-time_debit_start.tv_sec)*pow(10,6) + (time_debit_end.tv_usec - time_debit_start.tv_usec);
-    float debit = (float)size_file / time_debit.tv_usec;
-    printf("temps débit en micro sec : %d\n", time_debit.tv_usec);
-    printf("débit lors de la transmission : %f Mo/s\n", debit);
+    time_debit.tv_sec = (time_debit_end.tv_sec-time_debit_start.tv_sec);
+    float debit = (float)size_file / time_debit.tv_sec;
+    printf("temps débit en sec : %ld\n", time_debit.tv_sec);
+    printf("débit lors de la transmission : %f o/s\n", debit);
 
     close(data_descriptor);
     break;

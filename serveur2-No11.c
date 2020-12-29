fde7b1017b6919bd1a6d5ece480a34cf7fc95327
StevenINSA <>
2020-12-29 15:49:33 +0100

    fenêtre dynamique

--- serveur2-No11.c
+++ serveur2-No11.c
@@ -311,7 +311,7 @@ int main(int argc, char* argv[]){
 
           *shared_memory_seq=ack_max+1; //retransmission à partir du ACK max reçu
           printf("Timeout : retransmission à partir de %d\n",ack_max+1);
-          timeout.tv_usec = 2*timeout.tv_usec; //on sécurise le temps d'attente de retransmission car il y a congestion
+          timeout.tv_usec = 5*timeout.tv_usec; //on sécurise le temps d'attente de retransmission car il y a congestion
           timeout.tv_sec = 0; //lors d'un timeout, on augmente le rtt car congestion
 
           size_window = 1; //quand timeout, il y a congestion donc on remet la fenêtre à 1

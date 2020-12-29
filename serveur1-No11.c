e29deaff8a43b92fb8149662d2b93aca07b6e393
StevenINSA <>
2020-12-29 11:56:14 +0100

    srtt

--- serveur1-No11.c
+++ serveur1-No11.c
@@ -272,7 +272,7 @@ int main(int argc, char* argv[]){
           timeout.tv_usec = 3*srtt.tv_usec; //on attend 3 fois l'estimation avant de déclarer l'ACK comme perdu
           timeout.tv_sec = 0;
 
-          printf("estimation du SRTT : %d\n", srtt.tv_usec);
+          //printf("estimation du SRTT : %ld\n", srtt.tv_usec);
 
           if (ack_max < atoi(buffer_sequence)){
             ack_max = atoi(buffer_sequence);
@@ -292,7 +292,7 @@ int main(int argc, char* argv[]){
           if(atoi(buffer_sequence)==ack_precedent){
             //printf("RETRANSMISSION à partir de %d\n",ack_precedent+1);
             *shared_memory_seq=ack_precedent+1; //on renvoit à partir du ack dupliqué, nous avons vu que il n'y avait jamais que 2 acks dupliqués
-            timeout.tv_usec = 100000; //on sécurise le temps d'attente de retransmission
+            timeout.tv_usec = 3*timeout.tv_usec; //on sécurise le temps d'attente de retransmission
             timeout.tv_sec = 0;                                  //
           }
 
@@ -310,7 +310,7 @@ int main(int argc, char* argv[]){
         else { //si Timeout
           *shared_memory_seq=ack_max+1; //retransmission à partir du ACK max reçu
           printf("Timeout : retransmission à partir de %d\n",ack_max+1);
-          timeout.tv_usec = 100000; //on sécurise le temps d'attente de retransmission
+          timeout.tv_usec = 3*timeout.tv_usec; //on sécurise le temps d'attente de retransmission
           timeout.tv_sec = 0; //lors d'un timeout, on augmente le rtt car congestion
         }
       }//fin while

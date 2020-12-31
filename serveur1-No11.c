4c493ce851017e3d69fb717686f793b11bd254b8
StevenINSA <>
2020-12-31 11:56:02 +0100

    ajout dans le fichier principal

--- serveur1-No11.c
+++ serveur1-No11.c
@@ -12,7 +12,6 @@
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <signal.h>
-#define PAGESIZE 4096
 
 int main(int argc, char* argv[]){
 
@@ -155,32 +154,40 @@ int main(int argc, char* argv[]){
 
     printf("taille du fichier en octet : %d\n", size_file);
     fseek(file, 0, SEEK_SET);          //on replace le curseur au début;
-    char file_buffer[size_file];
-    //les octets lus sont stockés dans buffer_fichier
-    //size_t read_blocks = fread(file_buffer,1,500,fichier); //500 blocs de 1 octet
-    size_t read_blocks = fread(file_buffer,size_file,1,file); //on lit le fichier en un coup (1 bloc de taille_fichier octets)
-
-    if(read_blocks!=1){
-      perror("erreur lecture fichier");
-      ferror(file);
-    }
+    int bloc_size = 6000000;
     int packets_size = 1494; //pour arriver à une taille de 1500 octets avec les 6 du n° de séquence
     int packets_number = size_file/packets_size;
+    int tableau_acks[packets_number+1];
     int size_window=100;
-    printf("Nombre de paquets à envoyer au total : %d\n",packets_number+1);
+    int size_tab = bloc_size/packets_size;
+
+    if (size_file < size_tab) //si le fichier lu est moins grand que le tableau, on n'a pas à tout parcourir + pas de gestion de gros fichier à faire
+      size_tab = size_file;
+
+    char (*tableau)[1494]=(char (*)[1494]) mmap(NULL, size_tab*1494,PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1,0);
 
 
-    uint8_t *shared_memory_fils = mmap(NULL, PAGESIZE,          //pour que le parent envoie les fichiers tant que le fils écoute les acks
+    for(int i=0;i<size_tab;i++){
+      int read_blocks = fread(tableau[i],1494,1,file);
+
+      if(read_blocks!=1){
+        perror("erreur lecture fichier");
+        ferror(file);
+      }
+    }
+
+
+    uint8_t *shared_memory_fils = mmap(NULL, 2,          //pour que le parent envoie les fichiers tant que le fils écoute les acks
                                     PROT_READ | PROT_WRITE,
                                     MAP_SHARED | MAP_ANONYMOUS, -1,0);
     *shared_memory_fils = 1;
 
-    uint16_t *shared_memory_seq = mmap(NULL, PAGESIZE,           //pour que le fils mette à jour le n° de seq que le parent envoie
+    uint16_t *shared_memory_seq = mmap(NULL, packets_number,           //pour que le fils mette à jour le n° de seq que le parent envoie
                                     PROT_READ | PROT_WRITE,
                                     MAP_SHARED | MAP_ANONYMOUS, -1,0);
     *shared_memory_seq = 1;
 
-    uint16_t *shared_memory_window = mmap(NULL, PAGESIZE,
+    uint16_t *shared_memory_window = mmap(NULL, packets_number,
                                     PROT_READ | PROT_WRITE,
                                     MAP_SHARED | MAP_ANONYMOUS, -1,0);
 
@@ -219,7 +226,7 @@ int main(int argc, char* argv[]){
 
           //Segment auquel on rajoute en-tête
           memcpy(buffer_segment,buffer_sequence,6);
-          memcpy(buffer_segment+6,file_buffer+packets_size*(*shared_memory_seq-1),packets_size);
+          memcpy(buffer_segment+6,tableau[(*shared_memory_seq-1)%size_tab],packets_size);
 
           /*ENVOI PAQUET*/
           sendto(data_descriptor,buffer_segment,packets_size+6,0,(struct sockaddr *)&client1_addr,len);
@@ -245,6 +252,8 @@ int main(int argc, char* argv[]){
       int ack_precedent_2=0;
       int last_ack_max = 0;
       int last2_ack_max = 0;
+      int incr = 0;
+      int seuil=2000;
 
       /***RECEPTION DES ACKs***/
       while (ack_max != packets_number+1){
@@ -266,33 +275,50 @@ int main(int argc, char* argv[]){
           int size_seq = recvfrom(data_descriptor, bufferUDP_read_server, sizeof(bufferUDP_read_server), 0, (struct sockaddr *)&client1_addr, &len);
           memcpy(buffer_sequence, bufferUDP_read_server+3, size_seq-3); //+3 car les 3 premières valeurs sont pour le mot ACK
 
+          /*GESTION RTT*/
           gettimeofday(&time2, NULL);                                   //on recalcule une timeofday pour faire la différence avec le premier
           array_fils[atoi(buffer_sequence)] = time2.tv_usec + time2.tv_sec*pow(10,6);
           rtt.tv_usec = array_fils[atoi(buffer_sequence)] - array_pere[atoi(buffer_sequence)];
           srtt.tv_usec = alpha*srtt.tv_usec + (1-alpha)*rtt.tv_usec;
-
           timeout.tv_usec = 3*srtt.tv_usec; //on attend 3 fois l'estimation avant de déclarer l'ACK comme perdu
           timeout.tv_sec = 0;
 
-          //printf("estimation du SRTT : %ld\n", srtt.tv_usec);
-
+          /*GESTION FENETRE GLISSANTE*/
           if (ack_max < atoi(buffer_sequence)){ //si le ack que l'on reçoie est supérieur au ack max stocké, ack max devient ce ack
             ack_max = atoi(buffer_sequence);
-            //size_window = 100;
             *shared_memory_window=ack_max+size_window;
           }
 
+          /*GESTION LECTURE FICHIER*/
+          //printf("Position curseur %d\n",ftell(file));
+
+          if(size_file <= size_tab){ //si fichier non volumineux, on n'a pas à faire la suite
+            if(atoi(buffer_sequence)>seuil){
+              seuil=seuil+2000;
+              //printf("ack vaut : %d -> on rempli le buffer\n", atoi(buffer_sequence));
+              //printf("valeur de incr : %d\n", incr);
+
+              for (int i = 0; i < seuil ; i++){
+                //printf("valeur de incr : %d\n", incr);
+                fread(tableau[incr%size_tab],1494,1,file);
+                incr++;
+              }
+            }
+          }
+
+          /*GESTION ACKS DUPLIQUES*/
           if(atoi(buffer_sequence)==ack_precedent && atoi(buffer_sequence)==ack_precedent_2){
-            //size_window = 3;
+            //*shared_memory_window = ack_precedent+1;
             goto skip;
           }
 
+          /*GESTION ACKS DUPLIQUES*/
           if(atoi(buffer_sequence)==ack_precedent){
-            printf("Ack duppliqué : retransmission à partir de %d\n",ack_precedent+1);
+            //printf("Ack duppliqué : retransmission à partir de %d\n",ack_precedent+1);
             *shared_memory_seq=ack_precedent+1; //on renvoit à partir du ack dupliqué, nous avons vu que il n'y avait jamais que 2 acks dupliqués
             timeout.tv_usec = 3*timeout.tv_usec; //on sécurise le temps d'attente de retransmission
             timeout.tv_sec = 0;
-            //size_window = 3; //on a remarqué que le client1 ne perdait qu'un seul paquet. Au lieu d'en retransmettre 100, on n'en retransmet qu'un seul
+            //*shared_memory_window = ack_precedent+1; //on a remarqué que le client1 ne perdait qu'un seul paquet. Au lieu d'en retransmettre 100, on n'en retransmet qu'un seul
           }
 
           ack_precedent_2 = ack_precedent;

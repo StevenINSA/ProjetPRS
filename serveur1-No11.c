0fa24ce32cd9e8835d9a57cfe02ac1084f4554b4
StevenINSA <>
2020-12-31 13:33:20 +0100

    ajout dans le fichier principal

--- serveur1-No11.c
+++ serveur1-No11.c
@@ -253,8 +253,6 @@ int main(int argc, char* argv[]){
       int ack_max = 0;
       int ack_precedent=0;
       int ack_precedent_2=0;
-      int last_ack_max = 0;
-      int last2_ack_max = 0;
       int incr = 0;
       int seuil=2000;
 
@@ -336,27 +334,16 @@ int main(int argc, char* argv[]){
 
         } //FDISSET
         else { //si Timeout
-/*
-          if(last_ack_max == ack_max && last_ack_max == last2_ack_max){ //si le timeout a lieu sur le même ack que précédemment, on ne retransmet pas tout
-            timeout.tv_usec = 5*srtt.tv_usec;
-            timeout.tv_sec = 0;
-            goto skip2;                                                 //sécurité sur 2 ack car des bugs ont lieu lorsqu'on a un timeout et un ack dupliqué sur la même séquence
-          }
-*/
+
           *shared_memory_seq=ack_max+1; //retransmission à partir du ACK max reçu
 
-          timeout.tv_usec = 10*srtt.tv_usec; //on sécurise le temps d'attente de retransmission car il y a congestion
+          timeout.tv_usec = 20*srtt.tv_usec; //on sécurise le temps d'attente de retransmission car il y a congestion
           timeout.tv_sec = 0; //lors d'un timeout, on augmente le rtt car congestion
 
-          last2_ack_max = last_ack_max;
-          last_ack_max = ack_max;
           *shared_memory_window = ack_max+1 + size_window;
           printf("Timeout : retransmission à partir de %d\n",ack_max+1);
           //printf("taille de la fenêtre en timeout : %d\n", *shared_memory_window);
-/*
-          skip2:
-            continue;
-*/
+
         }
       }//fin while
 

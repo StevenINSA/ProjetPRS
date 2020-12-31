56f9c36b3d344eff7a57956a0378107930b1eecb
StevenINSA <>
2020-12-31 12:00:19 +0100

    ajout dans le fichier principal

--- serveur1-No11.c
+++ serveur1-No11.c
@@ -315,7 +315,7 @@ int main(int argc, char* argv[]){
           if(atoi(buffer_sequence)==ack_precedent){
             //printf("Ack duppliqué : retransmission à partir de %d\n",ack_precedent+1);
             *shared_memory_seq=ack_precedent+1; //on renvoit à partir du ack dupliqué, nous avons vu que il n'y avait jamais que 2 acks dupliqués
-            timeout.tv_usec = 3*timeout.tv_usec; //on sécurise le temps d'attente de retransmission
+            timeout.tv_usec = 3*srtt.tv_usec; //on sécurise le temps d'attente de retransmission
             timeout.tv_sec = 0;
             //*shared_memory_window = ack_precedent+1; //on a remarqué que le client1 ne perdait qu'un seul paquet. Au lieu d'en retransmettre 100, on n'en retransmet qu'un seul
           }
@@ -332,12 +332,14 @@ int main(int argc, char* argv[]){
 
           if(last_ack_max == ack_max && last_ack_max == last2_ack_max){ //si le timeout a lieu sur le même ack que précédemment, on ne retransmet pas tout
             //size_window = 3;
+            timeout.tv_usec = 5*srtt.tv_usec;
+            timeout.tv_sec = 0;
             goto skip2;                                                 //sécurité sur 2 ack car des bugs ont lieu lorsqu'on a un timeout et un ack dupliqué sur la même séquence
           }
 
           *shared_memory_seq=ack_max+1; //retransmission à partir du ACK max reçu
           printf("Timeout : retransmission à partir de %d\n",ack_max+1);
-          timeout.tv_usec = 5*timeout.tv_usec; //on sécurise le temps d'attente de retransmission car il y a congestion
+          timeout.tv_usec = 5*srtt.tv_usec; //on sécurise le temps d'attente de retransmission car il y a congestion
           timeout.tv_sec = 0; //lors d'un timeout, on augmente le rtt car congestion
 
           last2_ack_max = last_ack_max;

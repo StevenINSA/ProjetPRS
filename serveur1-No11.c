207502f18e32a21acd5b277eb9ff998b14db1785
StevenINSA <>
2021-01-09 20:58:18 +0100

    erreur

--- serveur1-No11.c
+++ serveur1-No11.c
@@ -248,12 +248,12 @@ int main(int argc, char* argv[]){
           //Remise à zéro des buffers
           memset(buffer_segment,0,sizeof(buffer_segment));
           memset(buffer_sequence,0,sizeof(buffer_sequence));
-          printf("\nnum seq avant mlock %d\n", *shared_memory_seq);
+          //printf("\nnum seq avant mlock %d\n", *shared_memory_seq);
           int lock = mlock(shared_memory_seq, packets_number);
           if (lock == -1){
             printf("erreur mlock\n");
           }
-          printf("num seq après mlock %d\n", *shared_memory_seq);
+          //printf("num seq après mlock %d\n", *shared_memory_seq);
           sprintf(buffer_sequence,"%d",*shared_memory_seq);
 
           //printf("Sequence number (from buffer_sequence) : %s\n",buffer_sequence);
@@ -263,7 +263,7 @@ int main(int argc, char* argv[]){
 
           memcpy(buffer_segment+6,tableau[(*shared_memory_seq-1)%size_tab],packets_size);
           munlock(shared_memory_seq, packets_number);
-          printf("num seq après munlock %d\n", *shared_memory_seq);
+          //printf("num seq après munlock %d\n", *shared_memory_seq);
 
           /*ENVOI PAQUET*/
           packets_size = 1494; //si une retransmission a lieu alors que l'on a envoyé le dernier segment, il faut réinitialiser packets_size

89aeef00d3678b4166c237b94f69d74ecf288e8f
StevenINSA <>
2021-01-10 23:22:51 +0100

    essai

--- serveur1-No11.c
+++ serveur1-No11.c
@@ -157,7 +157,7 @@ int main(int argc, char* argv[]){
     int bloc_size = 8000000;
     int packets_size = 1494; //pour arriver à une taille de 1500 octets avec les 6 octets du n° de séquence
     int packets_number = (size_file/packets_size)+1; //le nombre de segments que le serveur devra envoyer au client
-    int size_window = 50;
+    int size_window = 75;
     int size_tab = bloc_size/packets_size; //il faut que la dimension du tableau (collonne*lignes) ne dépasse pas 8 000 000
     printf("taille du tableau : %d\n", size_tab);
     printf("nombre de segments à envoyer : %d\n", packets_number);
@@ -338,9 +338,6 @@ int main(int argc, char* argv[]){
           if (ack_max < atoi(buffer_sequence)){ //si le ack que l'on reçoie est supérieur au ack max stocké, ack max devient ce ack
             ack_max = atoi(buffer_sequence);
 
-            if (size_window < 100) { //on augmente jusqu'à max size_window = 30
-              size_window = size_window + 10; //si on a une petite taille de fenêtre, on l'augmente de façon exponentielle
-            }
             *shared_memory_window=ack_max+size_window;  //et on fait glisser la fenêtre
 
             //printf("taille de la fenêtre en réception normale : %d\n", *shared_memory_window);
@@ -374,8 +371,7 @@ int main(int argc, char* argv[]){
 
             sendto(data_descriptor,buffer_segment,packets_size+6,0,(struct sockaddr *)&client1_addr,len);
 
-            size_window = 40;
-            *shared_memory_window=ack_precedent+size_window;
+
             *count_ack_memory = *count_ack_memory + 1;
             //printf("count ack memory : %d\n", *count_ack_memory);
 
@@ -437,7 +433,7 @@ int main(int argc, char* argv[]){
           timeout.tv_usec = 5*srtt.tv_usec; //on sécurise le temps d'attente de retransmission car il y a congestion (évite 2 timeout consécutifs)
           timeout.tv_sec = 0; //lors d'un timeout, on augmente le rtt car congestion
 
-          size_window = 40;
+
           *shared_memory_window = ack_max+1 + size_window; //on remet à jour la fenêtre
           printf("Timeout : retransmission à partir de %d\n",ack_max+1);
           //printf("taille de la fenêtre en timeout : %d\n", *shared_memory_window);
